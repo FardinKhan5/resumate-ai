b0849989b7af84f6623284163e2f7b5834e61353
FardinKhan5 <fardinkhan52003@gmail.com>
2025-08-30 16:00:18 +0530

    setup prettier

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,8 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View
-      className="flex-1 justify-center items-center bg-red-600"
-    >
+    <View className="flex-1 justify-center items-center bg-red-600">
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
