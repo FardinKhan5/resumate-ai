c1959d8c0326a7c2d774d1fbffb42081e798e2ca
FardinKhan5 <fardinkhan52003@gmail.com>
2025-09-12 19:17:29 +0530

    test

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,10 @@
 import Button from '@/components/Button';
 import Loader from '@/components/Loader';
 import { useAuth } from '@clerk/clerk-expo';
+import { Image } from 'expo-image';
 import { Redirect, useRouter } from 'expo-router';
 import { useColorScheme } from 'nativewind';
-import { Image, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 export default function Index() {
   const { toggleColorScheme, colorScheme } = useColorScheme();
@@ -18,18 +19,19 @@ export default function Index() {
     return <Redirect href={'./(tabs)'} />;
   }
   return (
-    <SafeAreaView className="flex-1 mx-auto bg-white dark:bg-black">
+    <SafeAreaView className="flex-1 bg-white dark:bg-black">
       <View className="p-8">
         <Text className="text-black font-bold text-xl dark:text-white">Resumate Ai</Text>
       </View>
       <View className="flex-1 justify-center items-center">
         <Image
           source={require('./../assets/images/logo.png')}
-          style={{ width: 400, height: 400, resizeMode: 'cover', position: 'absolute' }}
+          contentFit="cover"
+          style={{ flex: 1, width: '100%' }}
         />
       </View>
       <View className="flex-1 px-8 pt-12 rounded-t-2xl bg-gray-200 justify-between dark:bg-gray-900">
-        <Text className="text-center font-bold text-md leading-relaxed dark:text-white">
+        <Text className="text-center font-bold text-md leading-loose dark:text-white">
           Our AI-powered resume analyzer helps you stand out from the crowd. Get instant feedback
           and personalized suggestions to optimize your resume for applicant tracking systems (ATS)
           and recruiters. Stop guessing and start applying with confidence.
